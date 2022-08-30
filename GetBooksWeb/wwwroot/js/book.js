61377377e2f3d1cd03a51ada0e9dcc6b446787af
Ertugrul Sungur <ertugrullsngrr@gmail.com>
2022-08-30 12:10:53 +0300

    Edit UI Design

--- GetBooksWeb/wwwroot/js/book.js
+++ GetBooksWeb/wwwroot/js/book.js
@@ -20,11 +20,11 @@ function LoadDataTable() {
                 "class" : "text-center",
                 "render": function (data) {
                     return `
-                            <div class="btn-group" role="group">
-							    <a href="/Admin/Book/Upsert?id=${data}" class="btn btn-primary me-2">
+                            <div class="btn-group d-flex" role="group">
+							    <a href="/Admin/Book/Upsert?id=${data}" class="w-100 btn btn-primary me-2">
 								    <i class="bi bi-pencil-square"></i> Edit</a>
 
-							    <a  onClick=Delete('/Admin/Book/Delete/${data}') class="btn btn-danger">
+							    <a  onClick=Delete('/Admin/Book/Delete/${data}') class="w-100 btn btn-danger">
                                     <i class="bi bi-trash-fill"></i> Delete</a>
 						    </div>
                         `
